367f12263a6b2ffb7dddbed9f264e63a225f96fc
mesifta <116611664+mesifta@users.noreply.github.com>
2022-10-25 02:56:52 +0100

    Update idt.h

--- CPU/idt.h
+++ CPU/idt.h
@@ -1,1 +1,22 @@
 
+typedef struct {
+    unsigned short low_offset; /* Lower 16 bits of handler function address */
+    unsigned short selector; /* Kernel segment selector */
+    unsigned char always0;
+    /* First byte
+     * Bit 7: "Interrupt is present"
+     * Bits 6-5: Privilege level of caller (0=kernel..3=user)
+     * Bit 4: Set to 0 for interrupt gates
+     * Bits 3-0: bits 1110 = decimal 14 = "32 bit interrupt gate" */
+    unsigned char flags;
+    unsigned short high_offset; /* Higher 16 bits of handler function address */
+} __attribute__((packed)) idt_gate_t;
+
+typedef struct {
+    unsigned short limit;
+    unsigned int base;
+} __attribute__((packed)) idt_register_t;
+
+#define low_16(address) (unsigned short)((address) & 0xFFFF)
+#define high_16(address) (unsigned short)(((address) >> 16) & 0xFFFF)
+
