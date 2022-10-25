6c7086d5c6eda04801d254fd669c5b7734775e5a
mesifta <116611664+mesifta@users.noreply.github.com>
2022-10-25 03:01:14 +0100

    Update low_level.c

--- Util/low_level.c
+++ Util/low_level.c
@@ -1,1 +1,24 @@
+//receive byte to device control port register
+unsigned char port_byte_in(unsigned short port){
+	unsigned char result;
+	__asm__("in %%dx, %%al" : "=a" (result) : "d" (port));
+	return result;
+}
+
+//send byte to device control port register
+void port_byte_out(unsigned short port, unsigned char data){
+	__asm__("out %%al, %%dx" : : "a" (data), "d" (port));
+}
+
+//receive 2 bytes from device control port register
+unsigned short port_word_in(unsigned short port){
+	unsigned short result;
+	__asm__("in %%dx, %%ax" : "=a" (result) : "d" (port));
+	return result;
+}
+
+//send 2 bytes to device control port register
+void port_word_out(unsigned short port, unsigned short data){
+	__asm__("out %%ax, %%dx" : : "a" (data), "d" (port));
+}
 
