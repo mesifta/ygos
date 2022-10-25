9a573bd0698bc125e63c639f504e67cf677dd31c
mesifta <116611664+mesifta@users.noreply.github.com>
2022-10-25 02:52:06 +0100

    Update Kernel.c

--- Kernel/Kernel.c
+++ Kernel/Kernel.c
@@ -1,1 +1,21 @@
+#include "../Drivers/colors.h"
+#include "string.h"
+#include "../Util/stdint.h"
+
+extern char greet[];
+extern uint16_t *disk_buff_ptr;
+
+void start(){
+	isr_install();
+	init_isr_exc();
+	asm volatile("sti");
+	init_keyboard();
+	init_heap();
+	init_disk();
+	
+	print_s(greet);
+	prompt();
+    
+	return;
+}
 
