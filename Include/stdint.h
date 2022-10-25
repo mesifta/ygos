7fd6a03f73b7ac2fae201256c97e4a6fbb63992e
chaim green <116611664+mesifta@users.noreply.github.com>
2022-10-25 20:52:42 +0100

    Update stdint.h

--- Include/stdint.h
+++ Include/stdint.h
@@ -1,1 +1,13 @@
+#ifndef _STDINT_H
+#define _STDINT_H
 
+typedef char int8_t;
+typedef unsigned char uint8_t;
+typedef short int16_t;
+typedef unsigned short uint16_t;
+typedef int int32_t;
+typedef unsigned int uint32_t;
+
+#define NULL 0
+
+#endif
